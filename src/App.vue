67bcbe5dd7fa74840b0094cc4e77660441934e12
Murillo Aguiar <aguiarmurillo1@gmail.com>
2022-02-23 07:43:38 -0300

    Implementando o componente VagasFavoritas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
    <div>
 
+      <vagas-favoritas></vagas-favoritas>
+
       <!--<button @click="desmontarComponente()">Desmontar o componente conteudo</button>
       <topo-padrao @eventoParaPai="acao($event)" /> -->
 
@@ -8,7 +10,6 @@
       <topo-padrao @eventoParaPai="$event('Texto 1', 125)" />-->
 
       <!-- <topo-padrao :funcaoCallback="acao"/> -->
-
       <topo-padrao @navegar="componente = $event"/>
       
       <conteudo v-if="visibilidade" :conteudo="componente"></conteudo>
@@ -17,14 +18,16 @@
 </template>
 
 <script>
-import Conteudo from "@/components/layouts/Conteudo.vue";
-import TopoPadrao from "@/components/layouts/TopoPadrao.vue";
+import Conteudo from "@/components/layouts/Conteudo.vue"
+import TopoPadrao from "@/components/layouts/TopoPadrao.vue"
+import VagasFavoritas from "@/components/comuns/VagasFavoritas.vue"
 
 export default {
    name: "App",
    components: {
       Conteudo,
       TopoPadrao,
+      VagasFavoritas
    },
    data: ()=>({
       visibilidade: true,
